ead12fc0ffae423e4366c7df6fb5c7b1a48404ad
sm7177 <subrinamaharjan2020@gmail.com>
2024-09-10 20:22:09 +0545

    project=array

---
+++ script.js
@@ -0,0 +1,27 @@
+var svt = ["S.COUPS ", " Jeonghan ", " Joshua ", " Jun ", " Hoshi ", " Wonwoo ", " Woozi ", " DK ", " Mingyu ", " The 8", " Seungkwan ", " Vernon ", " Dino"]
+
+
+var obj = {
+    rname: "Choi Seungcheol",
+    sname: "S.Coups",
+    group: "Seventeen",
+}
+
+var arr = ["Cherry", "Angle", "Deer", "Cat", "Tiger", "Black Cat", "Rice", "Frog", "Dog", "Sun", "Orange", "Turtle", "Otter"]
+console.log("arr")
+
+
+document.getElementById("svt").innerHTML = svt
+
+document.getElementById("cherry").innerHTML = obj.rname + " professionally known by his stage name " + obj.sname + "is a South Korean rapper, singer and songwriter. Managed by Pledis Entertainment, he is the leader of the boy group " + obj.group + " and also the leader of its hip hop team."
+
+console.log(svt)
+
+console.log(arr)
+
+arr.push("pineapple")
+console.log(arr)
+
+arr.pop()
+console.log(arr)
+
